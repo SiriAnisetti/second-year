b8989eaadfb9b0eeb2b9b2cbdf7e6d1e665549d7
DarshiniKurasa <abhipriya.kurasa@gmail.com>
2024-10-25 18:03:02 +0530

    string concatenation

--- evenoroddchecker.py
+++ evenoroddchecker.py
@@ -2,7 +2,7 @@ def check_even_odd(num):
     if num % 2 == 0:  
         print(f"{num} is even")
     else:
-        print(num + " is odd") 
+        print(f"{num} is odd") 
     return None
 
 # Test the function
