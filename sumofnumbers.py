da3d0ff0d382b9c4b8a5dd854346dbd79ba3288a
DarshiniKurasa <abhipriya.kurasa@gmail.com>
2024-10-25 20:42:43 +0530

    Undefined Variable n and Total

--- sumofnumbers.py
+++ sumofnumbers.py
@@ -1,8 +1,8 @@
 def sum_of_list(lst):
     total = 0
     for num in lst:
-        total = total + n  
-    return Total  
+        total = total + num
+    return total  # Correct capitalization here 
 
 # Test the function
 print(sum_of_list([1, 2, '3', 4])) 
