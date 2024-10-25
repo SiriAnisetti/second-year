74f3c087c0558a7645c3386bf06c96b0c35c8515
srikavya15 <srikavya.karri15@gmail.com>
2024-10-25 20:31:47 +0530

    return variable not defined changes it known variable

--- reverse.py
+++ reverse.py
@@ -2,7 +2,7 @@ def reverse_string(s):
     reversed_str = ""
     for char in s:
         reversed_str = char+reversed_str
-    return reversed_string 
+    return reversed_str
 
 # Test the function
 print(reverse_string("Hacktoberfest"))  
