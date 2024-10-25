11a39667b229a8796fe74be4768e914856fee7ea
srikavya15 <srikavya.karri15@gmail.com>
2024-10-25 20:43:54 +0530

    Updated return variable
    returning Total instead of total

--- sumofnumbers.py
+++ sumofnumbers.py
@@ -2,7 +2,7 @@ def sum_of_list(lst):
     total = 0
     for num in lst:
         total = total + n  
-    return Total  
+    return total  
 
 # Test the function
 print(sum_of_list([1, 2, '3', 4])) 
