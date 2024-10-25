7df9c85d6617ebe451da31aa095f719a6cc690b8
DarshiniKurasa <abhipriya.kurasa@gmail.com>
2024-10-25 20:31:16 +0530

    Typo in the Function Return

--- reverse.py
+++ reverse.py
@@ -1,10 +1,10 @@
 def reverse_string(s):
     reversed_str = ""
     for char in s:
-        reversed_str = reversed_str + char 
-    return reversed_string 
+        reversed_str = char + reversed_str
+    return reversed_str  # Fix the return statement
 
 # Test the function
-print(reverse_string("Hacktoberfest"))  
-print(reverse_string(12345))         
-print(reverse_string(""))             
+print(reverse_string("Hacktoberfest"))
+print(reverse_string("12345"))  # This should be a string
+print(reverse_string(""))  # This will test with an empty string
