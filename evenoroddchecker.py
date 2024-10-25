6efead001a20957c27117a20a246582279c39bae
DarshiniKurasa <abhipriya.kurasa@gmail.com>
2024-10-25 18:16:42 +0530

    Handled Edge Cases

--- evenoroddchecker.py
+++ evenoroddchecker.py
@@ -1,10 +1,8 @@
 def check_even_odd(num):
-    #Checking
-    if num % 2 == 0:  
-        print(f"{num} is even")
-    else:
-        print(f"{num} is odd") 
-
-# Test the function
-check_even_odd(7) 
-check_even_odd(-4)  
+    try:
+        if num % 2 == 0: 
+            print(f"{num} is even")
+        else:
+            print(f"{num} is odd")
+    except TypeError:
+        print("Please enter a valid integer.")
