63e9ae60ff5583e0cbc016249ffba1fbfc98dd00
DarshiniKurasa <abhipriya.kurasa@gmail.com>
2024-10-25 20:44:13 +0530

    Undefined Variable n and Total in sumOfArrays.py

--- sumofnumbers.py
+++ sumofnumbers.py
@@ -5,6 +5,6 @@ def sum_of_list(lst):
     return total  # Correct capitalization here 
 
 # Test the function
-print(sum_of_list([1, 2, '3', 4])) 
+print(sum_of_list([1, 2, 3, 4])) 
 print(sum_of_list([1, 2, 3, 4, 5]))  
 print(sum_of_list([]))  
