5ef1035c0bdea5542d07676ca28fe0a7b6fe14cf
SiriAnisetti <sirianisetti88@gmail.com>
2024-10-27 10:29:42 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -2,8 +2,6 @@ def fibonacci(n):
     if n <= 1:
         return n
     return fibonacci(n - 1) + fibonacci(n - 2)
-
-# Test cases
-print(fibonacci(0))  # Expected: 0
-print(fibonacci(5))  # Expected: 5
-print(fibonacci(10)) # Expected: 55
+print(fibonacci(0))  
+print(fibonacci(5))  
+print(fibonacci(10)) 
