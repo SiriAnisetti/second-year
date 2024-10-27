bdb9a761342eee310c681297d6484afd35ea2af5
SiriAnisetti <sirianisetti88@gmail.com>
2024-10-27 10:33:46 +0530

    Update palidromeChecker.py

--- palidromeChecker.py
+++ palidromeChecker.py
@@ -1,8 +1,6 @@
 def is_palindrome(s):
     s = s.lower().replace(" ", "")
     return s == s[::-1]
-
-# Test cases
-print(is_palindrome("Madam"))            # Expected: True
-print(is_palindrome("Hello"))            # Expected: False
-print(is_palindrome("A man a plan a canal Panama"))  # Expected: True
+print(is_palindrome("Madam"))           
+print(is_palindrome("Hello"))            
+print(is_palindrome("A man a plan a canal Panama"))  
